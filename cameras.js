bca066434491a79cbf2c287c420137876344f86e
akutubidze <115596515+akutubidze@users.noreply.github.com>
2023-08-21 23:11:05 -0400

    Add files via upload

--- cameras.js
+++ cameras.js
@@ -92,11 +92,6 @@ const locations = [
         location: [40.720522816222655, -73.99218433131362],
         name: '<b>Sara D. Roosvelt Park</b><br>',
         time: "24Hour(Outdoor)"
-    },
-    {
-        location: [40.75379400655919, -73.98352412867399],
-        name: '<b>Briant Park</b><br>',
-        time: "7AM-11PM"
     }
 ]
 
@@ -120,11 +115,6 @@ const subways = axios('https://raw.githubusercontent.com/Willjfield/FOSS-for-Web
         }
     }).addTo(map).bringToBack();
 });
-
-axios('https://raw.githubusercontent.com/akutubidze/Public-space-cameras/main/map.geojson').then(resp => {
-    L.geoJSON(resp.data).addTo(map)
-})
-
 
 locations.forEach(function (locationObj) {
 
